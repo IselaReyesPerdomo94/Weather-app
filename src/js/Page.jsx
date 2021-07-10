351438bf0d2c5e331466d1f9d62df8d9dd7ed13b
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2021-07-10 10:38:17 -0700

    Fixing webpack config and adding new dependencies

---
+++ src/js/Page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import css from "../styles/Page";
+
+function Page() {
+  return (
+    <div>
+      <h1> Weather </h1>
+    </div>
+  );
+}
+
+export default Page;
---
+++ src/js/app.jsx
@@ -0,0 +1,7 @@
+import "babel-polyfill";
+import React from "react";
+import { render } from "react-dom";
+
+import Page from "./Page";
+
+render(<Page />, document.getElementById("app"));
